471f55e7aebaa03eb138fd54c92d87e45df99aef
Tomasz Szychlinski <s14354@pjwstk.edu.pl>
2020-03-29 17:28:31 +0200

    firts

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "test";
+        }
+
+    }
+}
